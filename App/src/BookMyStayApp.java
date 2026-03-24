eee900a88b5bdcedaf07aa6ba8602e4b2cbaf77f
vasun-debug <vn3339@srmist.edu.in>
2026-03-24 14:40:54 +0530

    Vasundara Re-added feature/UC1-WelcomePage

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -36,7 +36,7 @@ public class UseCase1HotelBookingApp {
      */
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("Welcome to the Hotel Booking Management System v1.0");
         System.out.println("System initialized successfully.");
     }
 }

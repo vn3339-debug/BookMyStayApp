176685782116567f0a2736dcd35a995a483c9e6e
vasun-debug <vn3339@srmist.edu.in>
2026-03-24 15:25:14 +0530

    Vasundara Added feature/UC5-BookingQueue

---
+++ App/src/Reservation.java
@@ -0,0 +1,29 @@
+/**
+ * ============================================================
+ * CLASS - Reservation
+ * ============================================================
+ *
+ * Represents a booking request made by a guest.
+ *
+ * @author Developer
+ * @version 5.0
+ */
+
+class Reservation {
+
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
